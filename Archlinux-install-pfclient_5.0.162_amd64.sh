45afa032e013ef166b2c6b9c70b1e2b3e215aeb1
abcd567a <abcd567@hotmail.com>
2022-02-14 02:15:18 -0500

    Update Archlinux-install-pfclient_5.0.162_amd64.sh

--- Archlinux-install-pfclient_5.0.162_amd64.sh
+++ Archlinux-install-pfclient_5.0.162_amd64.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-BINARY_VERSION=pfclient_5.0.162_amd64
+TARBALL=pfclient_5.0.162_amd64.tar.gz
 RESOURCE_FOLDER=/usr/share/pfclient
 sudo mkdir ${RESOURCE_FOLDER}
-echo "Downloading i386 binary tarball " ${BINARY_VERSION}.tar.gz "from Planefinder.net"
-sudo pacman -S --needed wget
-sudo wget -O ${RESOURCE_FOLDER}/${BINARY_VERSION}.tar.gz "http://client.planefinder.net/${BINARY_VERSION}.tar.gz"
-sudo tar zxvf  ${RESOURCE_FOLDER}/${BINARY_VERSION}.tar.gz -C ${RESOURCE_FOLDER}
-sudo cp ${RESOURCE_FOLDER}/pfclient /usr/bin/pfclient
+echo "Downloading tarball" ${TARBALL} "from Planefinder"
+sudo pacman -Sy --needed wget
+sudo wget -O ${RESOURCE_FOLDER}/${TARBALL} "http://client.planefinder.net/${TARBALL}"
+sudo bsdtar -xvpf ${RESOURCE_FOLDER}/${TARBALL} -C ${RESOURCE_FOLDER}
+
+sudo cp ${RESOURCE_FOLDER}/pfclient /usr/bin/
+
 
 echo "Creating config file pfclient-config.json"
 CONFIG_FILE=/etc/pfclient-config.json
@@ -16,6 +18,7 @@ sudo chmod 777 ${CONFIG_FILE}
 echo "Writing code to config file pfclient-config.json"
 /bin/cat <<EOM >${CONFIG_FILE}
 {}
+
 EOM
 
 sudo chmod 666 ${CONFIG_FILE}
@@ -23,7 +26,7 @@ sudo chmod 666 ${CONFIG_FILE}
 echo "Creating user pf to run service"
 sudo useradd --system pf
 
-echo "Creating Service file pfclient.service"
+echo "Creating Service file fr24feed.service"
 SERVICE_FILE=/usr/lib/systemd/system/pfclient.service
 sudo touch ${SERVICE_FILE}
 sudo chmod 777 ${SERVICE_FILE}
@@ -36,7 +39,7 @@ After=network-online.target
 Type=simple
 Restart=always
 LimitCORE=infinity
-RuntimeDirectory=pfclient
+RuntimeDirectory=fr24feed
 RuntimeDirectoryMode=0755
 ExecStartPre=-/bin/mkdir -p /var/log/pfclient
 ExecStartPre=-/bin/chown pf /var/log/pfclient
@@ -56,18 +59,15 @@ sudo systemctl restart pfclient
 
 echo " "
 echo " "
-echo -e "\e[32m INSTALLATION COMPLETED \e[39m"
+echo -e "\e[32mINSTALLATION COMPLETED \e[39m"
 echo -e "\e[32m=======================\e[39m"
-echo -e "\e[32m PLEASE DO FOLLOWING:\e[39m"
+echo -e "\e[32mPLEASE DO FOLLOWING:\e[39m"
 echo -e "\e[32m=======================\e[39m"
-echo -e "\e[32m SIGNUP:\e[39m"
-echo -e "\e[32m In your browser, go to web interface at\e[39m"
+echo -e "\e[32mSIGNUP:\e[39m"
+echo -e "\e[32mIn your browser, go to web interface at\e[39m"
 echo -e "\e[39m     http://$(ip route | grep -m1 -o -P 'src \K[0-9,.]*'):30053 \e[39m"
-echo -e "\e[32m Fill necessary details to sign up / sign in\e[39m"
-echo -e "\e[32m Use IP Address 127.0.0.1 and Port number 30005 when asked for these\e[39m"
-echo -e "\e[31m If it fails to save settings when you hit button [Complete Configuration],\e[39m"
-echo -e "\e[31m then restart pfclient by following command, and again hit [Complete Configuration] button\e[39m"
-echo "     sudo systemctl restart pfclient " 
+echo -e "\e[32mFill necessary details to sign up / sign in\e[39m"
+echo -e "\e[32mUse IP Address 127.0.0.1 and Port number 30005 when asked for these\e[39m"
 echo " "
 echo " "
 echo -e "\e[32mTo see status\e[39m sudo systemctl status pfclient"
