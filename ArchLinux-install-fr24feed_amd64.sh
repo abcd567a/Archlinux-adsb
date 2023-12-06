256a3439f54cb84a7bea5ed2fdf32c5d67eef744
abcd567a <abcd567@hotmail.com>
2023-12-06 04:42:37 -0500

    Update ArchLinux-install-fr24feed_amd64.sh

--- ArchLinux-install-fr24feed_amd64.sh
+++ ArchLinux-install-fr24feed_amd64.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+set -e
+trap 'echo "[ERROR] Error in line $LINENO when executing: $BASH_COMMAND"' ERR
 
 VERSION="1.0.44-0"
 RESOURCE_FOLDER=/usr/share/fr24feed
 sudo mkdir ${RESOURCE_FOLDER}
-echo "Downloading Tarball fr24feed_"${VERSION}"_amd64.tgz for amd64 from Flightradar24"
+echo -e "\e[1;32mDownloading Tarball fr24feed_"${VERSION}"_amd64.tgz for amd64 from Flightradar24 \e[0;39m"
 sudo pacman -S --needed wget
 sudo wget -O ${RESOURCE_FOLDER}/fr24feed_${VERSION}_amd64.tgz "https://repo-feed.flightradar24.com/linux_binaries/fr24feed_${VERSION}_amd64.tgz"
 sudo bsdtar -xvpf ${RESOURCE_FOLDER}/fr24feed_${VERSION}_amd64.tgz -C ${RESOURCE_FOLDER}
@@ -18,11 +20,11 @@ sudo chmod +x ${RESOURCE_FOLDER}/fr24feed-status
 sudo cp ${RESOURCE_FOLDER}/fr24feed-status /usr/bin/
 
 
-echo "Creating config file fr24feed.ini"
+echo -e "\e[1;32mCreating config file fr24feed.ini \e[0;39m"
 CONFIG_FILE=/etc/fr24feed.ini
 sudo touch ${CONFIG_FILE}
 sudo chmod 777 ${CONFIG_FILE}
-echo "Writing code to config file fr24feed.ini"
+echo -e "\e[1;32mWriting code to config file fr24feed.ini \e[0;39m"
 /bin/cat <<EOM >${CONFIG_FILE}
 receiver="avr-tcp"
 host="127.0.0.1:30002"
@@ -39,10 +41,14 @@ EOM
 
 sudo chmod 666 ${CONFIG_FILE}
 
-echo "Creating user fr24 to run service"
-sudo useradd --system fr24
+if [[ ! `id -u fr24` ]]; then
+echo -e "\e[1;32mCreating user fr24 to run fr24feed \e[0;39m"
+useradd --system fr24
+else
+echo -e "\e[1;32mUser fr24 already exists. Not creating it again \e[0;39m"
+fi
 
-echo "Creating Service file fr24feed.service"
+echo -e "\e[1;32mCreating Service file fr24feed.service \e[0;39m"
 SERVICE_FILE=/usr/lib/systemd/system/fr24feed.service
 sudo touch ${SERVICE_FILE}
 sudo chmod 777 ${SERVICE_FILE}
@@ -71,32 +77,32 @@ EOM
 
 sudo chmod 644 ${SERVICE_FILE}
 sudo systemctl enable fr24feed
+sudo fr24feed --signup
 sudo systemctl restart fr24feed
 
 echo " "
 echo " "
-echo -e "\e[32mINSTALLATION COMPLETED \e[39m"
-echo -e "\e[32m=======================\e[39m"
-echo -e "\e[32mPLEASE DO FOLLOWING:\e[39m"
-echo -e "\e[32m=======================\e[39m"
-echo -e "\e[32m(1) SIGNUP:\e[39m"
-echo -e "\e[32m   (a) If you already have a feeder key,\e[39m"
-echo -e "\e[33m       open file fr24feed.ini by following command and add fr24key:\e[39m"
-echo -e "\e[39m           sudo nano /etc/fr24feed.ini \e[39m"
-echo -e "\e[33m       Save (Ctrl+o) and Close (Ctrl+x) file fr24feed.ini \e[39m"
-echo -e "\e[33m       then restart fr24feed by following command:\e[39m"
-echo -e "\e[39m           sudo systemctl restart fr24feed \e[39m"
+echo -e "\e[1;32mINSTALLATION COMPLETED \e[0;39m"
+echo -e "\e[1;32m=======================\e[0;39m"
+echo -e "\e[1;32mPLEASE DO FOLLOWING:\e[0;39m"
+echo -e "\e[1;32m=======================\e[0;39m"
+echo -e "\e[1;32m(1) SIGNUP:\e[0;39m"
+echo -e "\e[1;32m   (a) If you already have a feeder key,\e[0;39m"
+echo -e "\e[1;33m       open file fr24feed.ini by following command and add fr24key:\e[0;39m"
+echo -e "\e[1;39m           sudo nano /etc/fr24feed.ini \e[0;39m"
+echo -e "\e[1;33m       Save (Ctrl+o) and Close (Ctrl+x) file fr24feed.ini \e[0;39m"
+echo -e "\e[1;33m       then restart fr24feed by following command:\e[0;39m"
+echo -e "\e[1;39m           sudo systemctl restart fr24feed \e[0;39m"
 echo " "
 
-echo -e "\e[32m   (b) Alternatively signup using following command\e[39m"
-echo -e "\e[39m         sudo nano fr24feed --signup \e[39m"
+echo -e "\e[1;32m   (b) Alternatively signup using following command\e[0;39m"
+echo -e "\e[1;39m         sudo fr24feed --signup \e[0;39m"
 echo " "
-echo -e "\e[32m(2) In your browser, go to web interface at\e[39m"
-echo -e "\e[39m     http://$(ip route | grep -m1 -o -P 'src \K[0-9,.]*'):8754 \e[39m"
+echo -e "\e[1;32m(2) In your browser, go to web interface at\e[0;39m"
+echo -e "\e[1;39m     http://$(ip route | grep -m1 -o -P 'src \K[0-9,.]*'):8754 \e[0;39m"
 echo " "
 echo " "
-echo -e "\e[32mTo see status\e[39m sudo systemctl status fr24feed"
-echo -e "\e[32mTo restart\e[39m    sudo systemctl restart fr24feed"
-echo -e "\e[32mTo stop\e[39m       sudo systemctl stop fr24feed"
-
-
+echo -e "\e[1;32mTo see status\e[39m sudo systemctl status fr24feed"
+echo -e "\e[1;32mTo restart\e[39m    sudo systemctl restart fr24feed"
+echo -e "\e[1;32mTo stop \e[39m      sudo systemctl stop fr24feed"
+echo -e "\e[0;39m"
